6de41f0b85e1406e40f61848836a55456d1953f8
Herman Adu <herman.adu@hotmail.com>
2020-02-18 13:33:55 +0000

    server update

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ const db = knex({
 	}
 });
 
-db.select('*').from('users').then(data => {
+/*db.select('*').from('users').then(data => {
 	console.log(data);
-})
+})*/
 
 const app = express();
 app.use(bodyParser.json());
