42cca39f204f8b3797f152d0f10b2a80f7546ccc
Herman Adu <herman.adu@hotmail.com>
2020-02-18 14:20:58 +0000

    removed comments

--- server.js
+++ server.js
@@ -30,26 +30,18 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// 1. create the root end-point that returns all users from database
 app.get('/', (req, res) => { res.send(db.users) })
 
-// 2.1 move signin function into controllers and pass in dependcies it  needs - dependency injection
 app.post('/signin', signin.handleSignin(db, bcrypt));
 
-// 3.1 move register function into controllers and pass in dependcies it  needs - dependency injection
 app.post('/register', register.handleRegister(db, bcrypt));
 
-// 4.1 move profile function into controllers and pass in dependcies it  needs - dependency injection
 app.get('/profile/:id', profile.handleProfile(db));
 
-// 5.1 move image function into controllers and pass in dependcies it  needs - dependency injection
-//app.put('/image', image.handleImage(db));
 app.put('/image', image.handleImage(db));
 
-// moved api call to back-end to hide key from browser/frontend
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
-// app started and listening on port 3000
 app.listen(process.env.PORT || 3000, () => {
 	console.log(`app is running on port ${process.env.PORT}`)
 });
